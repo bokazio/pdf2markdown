8104d1eeeb5ec3026d00e574b783df109ac9e1eb
Tim Zuercher <tim.zuercher@adfitech.com>
2016-11-01 10:02:07 -0500

    force jest to run all tests, fixes TravisCI not detecting passed tests

--- src/document/page/page.js
+++ src/document/page/page.js
@@ -5,9 +5,7 @@ class Page{
   async render(){
     this.viewport = this._pdfjsPage.getViewport(1);
     this.canvas = document.createElement('canvas')
-    console.log(1);
     this.canvas.width = this.viewport.width;
-    console.log(2);
     this.canvas.height = this.viewport.height;
     var renderContext = {
       canvasContext: this.canvas.getContext('2d'),
