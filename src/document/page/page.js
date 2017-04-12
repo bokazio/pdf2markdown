6ae0a58ca487bd0a3715cc3e584ea0fec8cb7c99
Tim Zuercher <tim.zuercher@eagles.oc.edu>
2017-04-12 13:40:55 -0500

    Document Page

--- src/document/page/page.js
+++ src/document/page/page.js
@@ -2,12 +2,11 @@ var fs =  require('fs');
 var JSCanvas = require('../../document_adapter/canvas_adapter/js_canvas/js_canvas.js');
 /**
  * Single page in the PDF file
- * Contains a VirtualCanvas for the definitive rendered version of the page elements
- * Contains MarkdownInformation
+ * Contains a JSCanvas for the definitive rendered version of the page elements
  */
 class Page{
   /**
-   * Renders the page to a virtual Canvas
+   * Renders the page to a virtual 'JSCanvas'
    * @return {async} async - Completes when page is rendered
    */
   static async render(pdfjsPage,number,documentId){
@@ -29,6 +28,7 @@ class Page{
       documentId: documentId,
     });
     
+    // Setup JSCanvas to use page and document
     JSCanvas.setPage(page.id);
     JSCanvas.setDocument(documentId);
     
@@ -37,37 +37,47 @@ class Page{
       viewport: viewport,
     };
     
+    // Get PDFJS going
     await pdfjsPage.render(renderContext);    
     
+    // Load any annotiations on the page
     await Page._loadAnnotations(pdfjsPage, page.id);
   }
-  
+  /**
+   * Get all annotations on a page
+   * @param  {Number} pdfjsPage 
+   * @param  {Number} pageId    current page id           
+   */
   static async _loadAnnotations(pdfjsPage, pageId){
+    // get all annotations PDFJS can get
     var annotations = await pdfjsPage.getAnnotations();
     var an = [];
     
-    var view = pdfjsPage.view;
-    
-    
-    
-    for(var a of annotations){
+    // get view to calculate position on page
+    var view = pdfjsPage.view;  
+       
+    for(var a of annotations){  
       
+      // get the bounding box of the annotation
       var rect = PDFJS.Util.normalizeRect([
-                      a.rect[0],
-                      view[3] - a.rect[1] + view[1],
-                      a.rect[2],
-                      view[3] - a.rect[3] + view[1]]);
-      
+        a.rect[0],
+        view[3] - a.rect[1] + view[1],
+        a.rect[2],
+        view[3] - a.rect[3] + view[1]]
+      );
+      // add annotation to the queue
       an.push({
         x1: rect[0],
         y1: rect[1],
         x2: rect[2],
         y2: rect[3],
         type: a.subtype,
+        // if it has a url use it otherwise get all destination information
         value: a.url ? a.url : JSON.stringify(a.dest),
         pageId: pageId   
       });
     }
+    // Bulk insert the annottions
     await DB.Annotation.bulkCreate(an);
   } 
   
